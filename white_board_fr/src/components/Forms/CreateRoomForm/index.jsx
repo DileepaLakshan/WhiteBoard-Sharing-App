6132af95a0d42915fe7561cbd0a14e47d1ec4868
Sandalu Dewthilini <150440179+SDewthilini@users.noreply.github.com>
2025-02-15 19:00:51 +0530

    connect the create room and join room

--- white_board_fr/src/components/Forms/CreateRoomForm/index.jsx
+++ white_board_fr/src/components/Forms/CreateRoomForm/index.jsx
@@ -1,13 +1,39 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
+const CreateRoomForm = ({uuid, socket, setUser}) => {
 
-const CreateRoomForm = () => {
+const [roomId, setRoomId] = useState(uuid());
+const [name , setName] = useState("");
+
+const navigate = useNavigate();
+
+const handleCreateRoom = (e) => {
+    e.preventDefault();
+
+    const roomData = {
+        name,
+        roomId,
+        userId: uuid(),
+        host: true,
+        presenter: true
+    };
+
+    setUser(roomData);
+    navigate(`/${roomId}`) ;
+    console.log(roomData) ;
+    socket.emit("userJoined", roomData);
+
+}
+
     return <form className="form  col-md-12 mt-5">
 
         <div className="form-group">
             <input
                 type="text"
                 className="form-control my-2"
-
+                value={name}
+                onChange= {(e)=>setName(e.target.value)}
                 placeholder="Enter your name"
             />
 
@@ -19,13 +45,18 @@ const CreateRoomForm = () => {
             <div className="input-group d-flex align-items-center jusitfy-content-center">
                 <input
                     type="text"
+                    value={roomId}
                     className="form-control my-2 border-0"
                     disabled
                     placeholder="Generate room code"
                 />
 
                 <div className="input-group-append ">
-                    <button className="btn btn-primary btn-sm me-1" type="button">
+                    <button 
+                    className="btn btn-primary btn-sm me-1" 
+                    onClick={()=>setRoomId(uuid())} 
+                    type="button"
+                    >
                         generate
                     </button>
                     <button className="btn btn-outline-danger btn-sm me-2" type="button">
@@ -35,7 +66,7 @@ const CreateRoomForm = () => {
                 </div>
             </div>
         </div>
-        <button type="submit" className="mt-4 btn-primary btn-block form-control">
+        <button type="submit" onClick={handleCreateRoom} className="mt-4 btn-primary btn-block form-control">
             Generate Room
         </button>
 
--- white_board_fr/src/components/Forms/JoinRoomForm/index.jsx
+++ white_board_fr/src/components/Forms/JoinRoomForm/index.jsx
@@ -1,5 +1,29 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
-const JoinRoomForm = () =>{
+
+const JoinRoomForm = ({uuid, socket, setUser}) =>{
+
+    const  [roomId, setRoomId] = useState("");
+    const [name, setName] = useState("");
+
+    const navigate = useNavigate();
+
+    const handleJoinRoom = (e)=>{
+        e.preventDefault();
+
+        const roomData={
+            name,
+            roomId,
+            userId: uuid(),
+            host: false,
+            presenter: false,
+        };
+        setUser(roomData);
+        navigate(`/${roomId}`);
+        socket.emit("userJoined", roomData);
+    }
+
     return (
         <form className="form  col-md-12 mt-5">
 
@@ -7,8 +31,9 @@ const JoinRoomForm = () =>{
             <input
                 type="text"
                 className="form-control my-2"
-
+                value={name}
                 placeholder="Enter your name"
+                onChange={(e)=>setName(e.target.value)}
             />
 
         </div>
@@ -20,14 +45,15 @@ const JoinRoomForm = () =>{
                 <input
                     type="text"
                     className="form-control my-2 "
-                    
+                    value={roomId}
                     placeholder="Enter room code"
+                    onChange={(e)=>setRoomId(e.target.value)}
                 />
 
                 
             
         </div>
-        <button type="submit" className="mt-4 btn-primary btn-block form-control">
+        <button type="submit" uuid={uuid} onClick={handleJoinRoom} className="mt-4 btn-primary btn-block form-control">
             Join Room
         </button>
 
